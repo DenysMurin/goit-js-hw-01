3d0beaaa6986faca71bfdf8f3231705afe08c173
DenysMurin <denys.murin.it@gmail.com>
2024-03-14 19:53:09 +0300

    change return/console.log
    change return/console.log

--- js/task-3.js
+++ js/task-3.js
@@ -3,8 +3,8 @@ function getElementWidth(content, padding, border) {
   const paddingNumber = Number.parseFloat(padding);
   const borderNumber = Number.parseFloat(border);
   const total = contentNumber + paddingNumber * 2 + borderNumber * 2;
-  console.log(total);
+  return total;
 }
-getElementWidth('50px', '8px', '4px');
-getElementWidth('60px', '12px', '8.5px');
-getElementWidth('200px', '0px', '0px');
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
